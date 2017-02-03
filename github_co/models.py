312ad5dbe12203f0d808e291fb2245dfe75b7dc0
Julian Urán <julianx@gmail.com>
2017-02-03 10:41:49 -0500

    Initial models.

--- github_co/models.py
+++ github_co/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Repos(models.Model):
+    name = models.CharField(max_length=100)
+    stars = models.IntegerField()
+    contributors = models.ManyToManyField('User', related_name='contributors')
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    repos = models.ManyToManyField(Repos)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('username',)
