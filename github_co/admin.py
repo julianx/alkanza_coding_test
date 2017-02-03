de3a4606fea00f26166e65045313ce1b1e50af90
Julian Urán <julianx@gmail.com>
2017-02-03 13:26:31 -0500

    Modded admin search field.

--- github_co/admin.py
+++ github_co/admin.py
@@ -4,7 +4,7 @@ from github_co.models import User, Repos, Location
 
 class UserAdmin(admin.ModelAdmin):
     raw_id_fields = ("repos", 'location')
-    search_fields = ('username', 'location')
+    search_fields = ('username', 'location__name')
     list_display = ('username', 'colombian')
 
 
