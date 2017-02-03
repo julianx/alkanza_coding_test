2d24a2226c0c55e477dd0d282c4033408996d3e4
Julian Urán <julianx@gmail.com>
2017-02-03 12:03:49 -0500

    Added model managers.

--- github_co/models.py
+++ github_co/models.py
@@ -5,13 +5,24 @@ class Location(models.Model):
     name = models.CharField(max_length=100)
 
 
+class RepoManager(models.Manager):
+    def get_popular_repos(self):
+        return self.filter().order_by('-stars')[10]
+
+    def get_colombian_repos(self):
+        return self.filter(User__location__name__contains="Colombia")
+
+    def get_popular_colombian_repos(self):
+        return self.filter().order_by('-contributors')[10]
+
+
 class Repos(models.Model):
     name = models.CharField(max_length=100)
     stars = models.IntegerField()
-    contributors = models.ManyToManyField('User', related_name='contributors')
+    contributors = models.ManyToManyField('User', related_name='contributors', null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return "%s, Star gazers: %d" % (self.name, self.stars)
 
 
 class User(models.Model):
