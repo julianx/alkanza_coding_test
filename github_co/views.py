8baa3363c782fc907b1b087d3d7abca641e5d4ad
Julian Urán <julianx@gmail.com>
2017-02-03 12:04:15 -0500

    Added crawlers.

--- github_co/views.py
+++ github_co/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from github import Github
+from github_co.models import User, Location, Repos
 
 # Create your views here.
+github = Github("julianx", "serlas3biergaF")
+
+def crawl_users_co():
+    users_co = github.search_users(query="location:colombia", sort='repositories', order='desc')[1]
+    for api_user in users_co:
+        user = User.objects.filter(name=api_user.login).first()
+        if not user:
+            user = User()
+            user.username = api_user.login
+
+            location = Location.objects.filter(name=api_user.location).first()
+            if not location:
+                location = Location(name=api_user.location)
+
+            user.location = location
+
+            collaborator_repos = api_user.get_repos()
+            for repo in collaborator_repos:
+                repository = Repos.objects.filter(name=repo.name).first()
+                if not repository:
+                    repository = Repos()
+                    repository.name = repo.name
+                    repository.stars = repo.stargazers_count
+                    repository.save()
+
+            user.save()
+
+
+
+def crawl popular_repos():
+    popular_repos = github.search_repositories(query="stars:>15000", sort="stars", order='desc')[:15]  # 162 total
+
+    for repo in popular_repos:
+        for collaborator in repo.get_collaborators():
+            user = User()
+
+            location = Location.objects.filter(name=collaborator.location).first()
+            if not location:
+                location = Location(name=collaborator.location)
+                location.save()
+
+            user.location = location
+            user.username = collaborator.login
+
+            collaborator_repos = collaborator.get_repos()
+            for repo in collaborator_repos:
+
+                repository = Repos.objects.filter(name=repo.name).first()
+                if not repository:
+                    repository = Repos()
+                    repository.name = repo.name
+                    repository.stars = repo.stargazers_count
+                repository.save()
+
+            user.save()
+
+
+#     NamedUser(login="enspdf")
+# users_co.get_repos()[0]
+#     Repository(full_name="enspdf/acat")
+#     Repository(full_name="enspdf/acat").name
+#         'acat'
+#     reposample.stargazers_count
+#         0
+#     reposample.get_collaborators()
+#     for i in reposample.get_contributors():
+#         print("+1")
